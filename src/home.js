20321a6d64fef153673a765d5334dd970269ff6e
John West <contactjohnwest@gmail.com>
2022-04-07 20:11:23 -0700

    Add home.js page

--- src/home.js
+++ src/home.js
@@ -3,7 +3,98 @@ let contentDiv = document.querySelector('#content');
 
 function createHome() {
     createNavBar();
+    createMainContent();
+    createFooter();
 }
+
+function createMainContent() {
+    // restaurant title and image
+    let main = createDiv('main');
+    let contentContainer = createDiv('content-container');
+    let restaurantNameContainer = createDiv('restaurant-name-container');
+    restaurantNameContainer.innerHTML = 'Jean-Paul\'s Pizzeria';
+    let mainImage = document.createElement('img');
+    mainImage.setAttribute('id', 'main-image');
+    mainImage.setAttribute('src', './images/hoja-studio-Thw3nleO3cM-unsplash.jpeg');
+    mainImage.setAttribute('alt', 'image of pizza chef');
+    
+    // review section
+    let reviewContainer = document.createElement('div');
+    reviewContainer.setAttribute('class', 'main-small-container');
+    let reviewDescription = document.createElement('div');
+    reviewDescription.setAttribute('class', 'review-description');
+    reviewDescription.innerHTML = 'My partner and I ordered the 12" Pepperoni ($17) and CBR (chicken bacon ranch) ($23) pizzas. It is relatively pricy here compared to other pizza places. But, they definitely win with the quality of their pizzas! If you\'re a fan of impressive cheese pulls, this place certainly doesn\'t disappoint. The crust was super fresh and fluffy with a slight crunch.';
+    let reviewPersonName = document.createElement('div');
+    reviewPersonName.setAttribute('class', 'review-person-name');
+    reviewPersonName.innerHTML = '- Marty Banks';
+    reviewContainer.appendChild(reviewDescription);
+    reviewContainer.appendChild(reviewPersonName);
+
+    // hours section
+    let hoursContainer = document.createElement('div');
+    hoursContainer.setAttribute('class', 'main-small-container');
+    let hoursTitle = createDiv('hours-title');
+    hoursTitle.innerHTML = 'Hours';
+    let weeklyHours = createDiv('weekly-hours');
+    let sunday = document.createElement('div');
+    sunday.innerHTML = 'Sunday: 9am - 8pm';
+    weeklyHours.appendChild(sunday);
+    let monday = document.createElement('div');
+    monday.innerHTML = 'Monday: 8am - 9pm';
+    weeklyHours.appendChild(monday);
+    let tuesday = document.createElement('div');
+    tuesday.innerHTML = 'Tuesday: 8am - 9pm';
+    weeklyHours.appendChild(tuesday);
+    let wednesday = document.createElement('div');
+    wednesday.innerHTML = 'Wednesday: 8am - 10pm';
+    weeklyHours.appendChild(wednesday);
+    let thursday = document.createElement('div');
+    thursday.innerHTML = 'Thursday: 8am - 10pm';
+    weeklyHours.appendChild(thursday);
+    let friday = document.createElement('div');
+    friday.innerHTML = 'Friday: 8am - 10pm';
+    weeklyHours.appendChild(friday);
+    let saturday = document.createElement('div');
+    saturday.innerHTML = 'Saturday: 9am - 8pm';
+    weeklyHours.appendChild(saturday);
+
+    hoursContainer.appendChild(hoursTitle);
+    hoursContainer.appendChild(weeklyHours);
+    
+    // location section
+    let locationContainer = document.createElement('div');
+    locationContainer.setAttribute('class', 'main-small-container');
+    let locationTitle = createDiv('location-title');
+    locationTitle.innerHTML = 'Location';
+    let locationAddress = createDiv('location-address');
+    locationAddress.innerHTML = '123 Country Lane, San Jose, CA';
+    
+    locationContainer.appendChild(locationTitle);
+    locationContainer.appendChild(locationAddress);
+
+
+    // append all sections to contentContainer
+    contentDiv.appendChild(main);
+    main.appendChild(contentContainer);
+    contentContainer.appendChild(restaurantNameContainer);
+    contentContainer.appendChild(mainImage);
+    contentContainer.appendChild(reviewContainer);
+    contentContainer.appendChild(hoursContainer);
+    contentContainer.appendChild(locationContainer);
+
+}
+
+
+function createDiv(idName) {
+    let div = document.createElement('div');
+    div.setAttribute('id', idName);
+    return div;
+}
+
+
+
+export default createHome;
+
 
 function createNavBar() {
     let navDiv = createDiv('navbar');
@@ -14,72 +105,26 @@ function createNavBar() {
     let divContact = createDiv('contact');
     divContact.innerHTML = 'Contact';
 
+
     navDiv.appendChild(divHome);
     navDiv.appendChild(divMenu);
     navDiv.appendChild(divContact);
+
+    contentDiv.appendChild(navDiv);
 }
 
-function createDiv(idName) {
-    let div = document.createElement('div');
-    div.setAttribute('id', idName);
-    return div;
+function createFooter() {
+    let footer = createDiv('footer');
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = 'Copyright <span>&copy;</span> 2022 John West';
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', 'https://github.com/contactjw');
+    anchor.setAttribute('target', '_blank');
+    anchor.innerHTML = '<img src="./images/icons8-github.svg" alt="github icon" id="github-image">';
+
+    footer.appendChild(paragraph);
+    footer.appendChild(anchor);
+
+    contentDiv.appendChild(footer);
 }
-
-
-export default createHome;
-
-
-/* <div id="navbar">
-            <div id="home">Home</div>
-            <div id="menu">Menu</div>
-            <div id="Contact">Contact</div>
-        </div>
-
-        <div id="main">
-            <div id="content-container">
-
-                <div id="restaurant-name-container">
-                    Jean-Paul's Pizzeria
-                </div>
-
-                <img id="main-image" src="./images/hoja-studio-Thw3nleO3cM-unsplash.jpg" alt="image of pizza chef">
-    
-                <div class="main-small-container">
-                    <div class="review-description">
-                        My partner and I ordered the 12" Pepperoni ($17) and CBR (chicken bacon ranch) ($23) pizzas. It is relatively pricy here compared to other pizza places. But, they definitely win with the quality of their pizzas! If you're a fan of impressive cheese pulls, this place certainly doesn't disappoint. The crust was super fresh and fluffy with a slight crunch. 
-                    </div>
-                    <div class="review-person-name">
-                        - Marty Banks
-                    </div>
-                </div>
-    
-                <div class="main-small-container">
-                    <div id="hours-title">
-                        Hours
-                    </div>
-                    <div id="weekly-hours">
-                        <div class="weekday">Sunday: 9am - 8pm</div>
-                        <div class="weekday">Monday: 8am - 9pm</div>
-                        <div class="weekday">Tuesday: 8am - 9pm</div>
-                        <div class="weekday">Wednesday: 8am - 10pm</div>
-                        <div class="weekday">Thursday: 8am - 10pm</div>
-                        <div class="weekday">Friday: 8am - 10pm</div>
-                        <div class="weekday">Saturday: 9am - 8pm</div>
-                    </div>
-                </div>
-    
-                <div class="main-small-container">
-                    <div id="location-title">
-                        Location
-                    </div>
-                    <div id="location-address">
-                        123 Country Lane, San Jose, CA
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div id="footer">
-            <p>Copyright <span>&copy;</span> 2022 John West</p>
-            <a href="https://github.com/contactjw" target="_blank"><img src="./images/icons8-github.svg" alt="github icon" id="github-image"></a>
-        </div> */
+        
