2e883de46c6fbddf5b7a64276a5d546201030b4f
John West <contactjohnwest@gmail.com>
2022-04-08 11:24:52 -0700

    Add style changes to main content container'

--- src/home.js
+++ src/home.js
@@ -2,9 +2,7 @@ let contentDiv = document.querySelector('#content');
 
 
 function createHome() {
-    createNavBar();
     createMainContent();
-    createFooter();
 }
 
 function createMainContent() {
@@ -92,39 +90,6 @@ function createDiv(idName) {
 }
 
 
+export {createHome, createDiv};
 
-export default createHome;
-
-
-function createNavBar() {
-    let navDiv = createDiv('navbar');
-    let divHome = createDiv('home');
-    divHome.innerHTML = 'Home';
-    let divMenu = createDiv('menu');
-    divMenu.innerHTML = 'Menu';
-    let divContact = createDiv('contact');
-    divContact.innerHTML = 'Contact';
-
-
-    navDiv.appendChild(divHome);
-    navDiv.appendChild(divMenu);
-    navDiv.appendChild(divContact);
-
-    contentDiv.appendChild(navDiv);
-}
-
-function createFooter() {
-    let footer = createDiv('footer');
-    let paragraph = document.createElement('p');
-    paragraph.innerHTML = 'Copyright <span>&copy;</span> 2022 John West';
-    let anchor = document.createElement('a');
-    anchor.setAttribute('href', 'https://github.com/contactjw');
-    anchor.setAttribute('target', '_blank');
-    anchor.innerHTML = '<img src="./images/icons8-github.svg" alt="github icon" id="github-image">';
-
-    footer.appendChild(paragraph);
-    footer.appendChild(anchor);
-
-    contentDiv.appendChild(footer);
-}
         
--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
-import createHome from "./home";
+import {createHome, createDiv} from "./home";
+
+let contentDiv = document.querySelector('#content');
 
 console.log('inside of index.js')
+
+//initialize the site.. always need to run this first
+createNavBar();
 createHome();
+createFooter();
+
+
+
+
+
+function createNavBar() {
+    let navDiv = createDiv('navbar');
+    let divHome = createDiv('home');
+    divHome.innerHTML = 'Home';
+    let divMenu = createDiv('menu');
+    divMenu.innerHTML = 'Menu';
+    let divContact = createDiv('contact');
+    divContact.innerHTML = 'Contact';
+
+
+    navDiv.appendChild(divHome);
+    navDiv.appendChild(divMenu);
+    navDiv.appendChild(divContact);
+
+    contentDiv.appendChild(navDiv);
+}
+
+function createFooter() {
+    let footer = createDiv('footer');
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = 'Copyright <span>&copy;</span> 2022 John West';
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', 'https://github.com/contactjw');
+    anchor.setAttribute('target', '_blank');
+    anchor.innerHTML = '<img src="./images/icons8-github.svg" alt="github icon" id="github-image">';
+
+    footer.appendChild(paragraph);
+    footer.appendChild(anchor);
+
+    contentDiv.appendChild(footer);
+}
